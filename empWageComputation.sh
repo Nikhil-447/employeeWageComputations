d40cc61a3e629d88f321cafa7fc784718a93aa14
Nikky-kallaje <nkallaje447@gmail.com>
2020-07-02 23:28:32 +0530

    added code for monthly emp wage calculation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,12 +2,16 @@
 
 echo "Welcome to Employee Wage Computation Program"
 
-isPartTime=1
-isFullTime=2
-EmpRatePerHr=20
+isPartTime=1;
+isFullTime=2;
+EmpRatePerHr=20;
+day=0;
+dailyalary=0;
+monthlySalary=0;
 
+for (( day=0; day <=20 ; day++ ))
+do
 empCheck=$((RANDOM%3))
-
 case $empCheck in
 
         $isPartTime)
@@ -24,5 +28,6 @@ case $empCheck in
 
 esac
 
-        salary=$(( $EmpRatePerHr * $empHrs ))
-echo $salary
+        dailyalary=$(( $EmpRatePerHr * $empHrs ))
+        monthlySalary=$(( $monthlySalary + $dailyalary ))
+done
